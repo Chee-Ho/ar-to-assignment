dfaa0f287f4fc829f3702dcd7fb6c694ae42c0ed
nextacademy <nextacademy@email.com>
2015-09-29 19:19:23 +0800

    linked model to database tasks table

--- todo.rb
+++ todo.rb
@@ -1,18 +1,10 @@
 require_relative 'config/application'
-require_relative 'app/models/task'
+require 'byebug'
 
-#puts "Put your application code in #{File.expand_path(__FILE__)}"
+byebug
+puts "Put your application code in #{File.expand_path(__FILE__)}"
 #write the code for the user input here.
 #call method to list, add, delete from app/model/task
 
-puts "Do you want to list, add, delete task?"
-command = $stdin.gets.chomp
-
-case command
-when "list"
-	Task.list(true)
-when "add"
-	
-end
 
 
