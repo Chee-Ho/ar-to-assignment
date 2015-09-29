d8e03f501ddbc6e8e8dd46b78e332875a701dc1c
nextacademy <nextacademy@email.com>
2015-09-29 19:58:36 +0800

    updated simple UI

--- todo.rb
+++ todo.rb
@@ -1,10 +1,28 @@
 require_relative 'config/application'
 require 'byebug'
 
-byebug
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+command = ARGV.shift
+string = ARGV.join(" ")
+
+#puts "Put your application code in #{File.expand_path(__FILE__)}"
 #write the code for the user input here.
 #call method to list, add, delete from app/model/task
-
-
+case command
+when "list"
+	puts "list"
+when "new"
+	puts "create new task"
+when "complete"
+	puts "set task to complete"
+when "delete"
+	puts "delete tasks"
+else
+	puts "Invalid Command"
+	puts "Available Command"
+	puts "list - list all task"
+	puts "new [task name]- create new task"
+	puts "complete [id] - set task to complete"
+	puts "delete [id] - delete the task"
+end
 
